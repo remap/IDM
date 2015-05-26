1c7a10ef3ec1078f2264bfae23ee8b0ca2ee1e64
Jeff Burke <jburke@ucla.edu>
2015-05-26 12:08:26 -0700

    update for new images

--- examples/collage-nerris/js/jbCollage.js
+++ examples/collage-nerris/js/jbCollage.js
@@ -11,76 +11,81 @@ function getRandomSubarray(arr, size) {
     return shuffled.slice(0, size);
 };
 
-function getFiles() {
-    var files; 
-    $.getJSON( "getImages.php", 
-    function( data ) {
-      files = data;  
-      console.log(JSON.stringify(data));    
-    });
-    return files; 
-};
+// ** TEMPORARY - Flatten one path deep only, to use all images in all places, 
+//    since there are not very many files per wall. 
+// We exclude main/ because all of the files are large
+//
+function flattenFileNames(allfiles, themefiles) {
+    for (p in allfiles) {
+        if (p=="main") continue;
+        for (k in allfiles[p]) {
+            themefiles.push(p+"/"+allfiles[p][k]);
+        }
+    } 
+}
 
-function loadImages(theme) {
+function loadImages(theme, files) {
 
     console.log("loadImages", theme); 
     
-    var MAX_IMAGES = 6;  // max images per theme
+    // The following is a temporary step to get more files per collage by using all subdirectories
+    // Otherwise, could just pick the theme files here
+    // themefiles = files[theme]
+    //
+    var themefiles=[];  // output
+    flattenFileNames(files[theme], themefiles); 
+    console.log("themefiles", themefiles);     
     
-    // Array of [0, 1, ..., MAX_IMAGES-1] 
-    var IDX_ARRAY = []
-    for (var i=0; i < MAX_IMAGES; i++) IDX_ARRAY.push(i); 
-    
-    function getRandomCount() {return Math.floor(Math.random()*(MAX_IMAGES-2))+2; }
+    function getRandomCount(m,n) {return Math.min(n, Math.floor(Math.random()*(themefiles.length-m))+m ); }
     
     // WING LEFT
     var target = document.getElementById("collage-WingL"); 
-    var numImages = getRandomCount(); // How many images? 
-    var images = getRandomSubarray(IDX_ARRAY, numImages);
+    var numImages = getRandomCount(4,10); // How many images? 
+    var images = getRandomSubarray(themefiles, numImages);
     th = Math.floor(595/(numImages/2)); // target height for this collage
     target.setAttribute("targetHeight", th.toString());
     for (var k in images) {
         console.log(k);
-        target.innerHTML += sprintf("<img src='images/%s/%d.jpg' />", theme, images[k]); 
+        target.innerHTML += sprintf("<img src='images/%s/%s' />", theme, images[k]); 
     }
 
 
     // WING RIGHT
     target = document.getElementById("collage-WingR"); 
-    numImages = getRandomCount();
-    images = getRandomSubarray(IDX_ARRAY, numImages);
+    numImages = getRandomCount(4,10);
+    images = getRandomSubarray(themefiles, numImages);
     th = Math.floor(595/(numImages/2)); 
     target.setAttribute("targetHeight", th.toString());
     for (var k in images) {
-        target.innerHTML += sprintf("<img src='images/%s/%d.jpg' />", theme, images[k]); 
+        target.innerHTML += sprintf("<img src='images/%s/%s' />", theme, images[k]); 
     }
     
     // REAR LEFT
     target = document.getElementById("collage-RearL"); 
-    numImages = getRandomCount();
-    images = getRandomSubarray(IDX_ARRAY, numImages);
+    numImages = getRandomCount(4,6);
+    images = getRandomSubarray(themefiles, numImages);
     th = Math.floor(595/(numImages/2));
     target.setAttribute("targetHeight", th.toString());
     for (var k in images) {
-        target.innerHTML += sprintf("<img src='images/%s/%d.jpg' />", theme, images[k]); 
+        target.innerHTML += sprintf("<img src='images/%s/%s' />", theme, images[k]); 
     }
 
     // REAR RIGHT
     target = document.getElementById("collage-RearR"); 
-    numImages = getRandomCount();
-    images = getRandomSubarray(IDX_ARRAY, numImages);
+    numImages = getRandomCount(4,6);
+    images = getRandomSubarray(themefiles, numImages);
     th = Math.floor(595/(numImages/2));
     for (var k in images) {
-        target.innerHTML += sprintf("<img src='images/%s/%d.jpg' />", theme, images[k]); 
+        target.innerHTML += sprintf("<img src='images/%s/%s' />", theme, images[k]); 
     }
 
     // MAIN_WALL
     target = document.getElementById("collage-MainWall"); 
-    numImages = getRandomCount();
-    images = getRandomSubarray(IDX_ARRAY, numImages);
+    numImages = getRandomCount(10,25);
+    images = getRandomSubarray(themefiles, numImages);
     th = Math.floor(595/(numImages/2));
     for (var k in images) {
-        target.innerHTML += sprintf("<img src='images/%s/%d.jpg' />", theme, images[k]); 
+        target.innerHTML += sprintf("<img src='images/%s/%s' />", theme, images[k]); 
     }
 
 
